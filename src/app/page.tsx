f74eeb30e7d68d33768628875094c5c7c061d88e
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2025-09-09 13:26:58 +0200

    Add: Redirect for login/signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 import { auth } from "@/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const [loading, setLoading] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      if (firebaseUser) return router.push("/dashboard");
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [router]);
 
   const handleLogin = async () => {
     try {
       const userCred = await signInWithEmailAndPassword(auth, email, password);
-      console.log("Signed in:", userCred.user);
       if (!userCred) return;
+      router.push("/dashboard");
     } catch (err) {
       console.error(err);
     }
@@ -20,31 +31,37 @@ export default function LoginPage() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center bg-[#ffffff13] p-10 rounded-lg">
         <h1>Budget Hero</h1>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-        />
-        <button
-          className="bg-[#ffffff1d] w-full rounded-sm py-1 hover:bg-[#0000002a] cursor-pointer"
-          onClick={handleLogin}
-        >
-          Login
-        </button>
-        <a href="/signup">Not a member?</a>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
+            />
+            <button
+              className="bg-[#ffffff1d] w-full rounded-sm py-1 hover:bg-[#0000002a] cursor-pointer"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+            <a href="/signup">Not a member?</a>
+          </>
+        )}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 import { auth, db } from "@/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useRouter } from "next/navigation";
 
 export default function SignupPage() {
+  const [loading, setLoading] = useState(true);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      if (firebaseUser) return router.push("/dashboard");
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [router]);
 
   const handleSignup = async () => {
     try {
@@ -23,8 +37,7 @@ export default function SignupPage() {
         name: username,
         createdAt: serverTimestamp(),
       });
-
-      console.log("User signed up and Firestore profile created:", user.uid);
+      useRouter().push("/dashboard");
     } catch (error) {
       console.error(error);
     }
@@ -34,40 +47,46 @@ export default function SignupPage() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center bg-[#ffffff13] p-10 rounded-lg">
         <h1>Budget Hero</h1>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-        />
-        <input
-          type="username"
-          name="username"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-        />
-        <button
-          className="bg-[#ffffff1d] w-full rounded-sm py-1 hover:bg-[#0000002a] cursor-pointer"
-          onClick={handleSignup}
-        >
-          Sign up
-        </button>
-        <a href="/">Already a member?</a>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
+            />
+            <input
+              type="username"
+              name="username"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
+            />
+            <button
+              className="bg-[#ffffff1d] w-full rounded-sm py-1 hover:bg-[#0000002a] cursor-pointer"
+              onClick={handleSignup}
+            >
+              Sign up
+            </button>
+            <a href="/">Already a member?</a>
+          </>
+        )}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
