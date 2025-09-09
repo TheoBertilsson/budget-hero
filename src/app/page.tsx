f0b434212b5056af0f956d27f5bd2d036f1557f9
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2025-09-09 14:23:12 +0200

    Add: Theme colors and background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-import { auth } from "@/firebase";
+import { auth } from "@/lib/firebase";
 import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-  const [loading, setLoading] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
@@ -13,7 +12,6 @@ export default function LoginPage() {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
       if (firebaseUser) return router.push("/dashboard");
-      setLoading(false);
     });
     return () => unsubscribe();
   }, [router]);
@@ -28,44 +26,46 @@ export default function LoginPage() {
     }
   };
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center bg-[#ffffff13] p-10 rounded-lg">
-        <h1>Budget Hero</h1>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email"
-              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-            />
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-            />
-            <button
-              className="bg-[#ffffff1d] w-full rounded-sm py-1 hover:bg-[#0000002a] cursor-pointer"
-              onClick={handleLogin}
-            >
-              Login
-            </button>
-            <a href="/signup">Not a member?</a>
-          </>
-        )}
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-[url(../static/budgetHero.png)] bg-cover bg-foreground">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center bg-background  p-10 rounded-lg">
+        <h1 className="text-text-primary text-2xl font-bold">Budget Hero</h1>
+
+        <>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="bg-foreground/20 text-text-primary px-2 py-1 rounded-sm"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="bg-foreground/20 text-text-primary px-2 py-1 rounded-sm"
+          />
+          <button
+            className="bg-[#10B981] text-text-secondary font-semibold w-full rounded-sm p-1 text-lg hover:bg-victory-green/80 active:bg-victory-green/75 cursor-pointer"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+          <a
+            href="/signup"
+            className="hover:underline hover:underline-offset-4 cursor-pointer"
+          >
+            Not a member?
+          </a>
+        </>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-text-secondary"
           href=""
           target="_blank"
         >
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { auth, db } from "@/firebase";
+import { auth, db } from "@/lib/firebase";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
@@ -9,7 +9,6 @@ import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 
 export default function SignupPage() {
-  const [loading, setLoading] = useState(true);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -18,7 +17,6 @@ export default function SignupPage() {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
       if (firebaseUser) return router.push("/dashboard");
-      setLoading(false);
     });
     return () => unsubscribe();
   }, [router]);
@@ -44,53 +42,55 @@ export default function SignupPage() {
   };
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center bg-[#ffffff13] p-10 rounded-lg">
-        <h1>Budget Hero</h1>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email"
-              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-            />
-            <input
-              type="username"
-              name="username"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Username"
-              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-            />
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              className="bg-[#ffffff1d] px-2 py-1 rounded-sm"
-            />
-            <button
-              className="bg-[#ffffff1d] w-full rounded-sm py-1 hover:bg-[#0000002a] cursor-pointer"
-              onClick={handleSignup}
-            >
-              Sign up
-            </button>
-            <a href="/">Already a member?</a>
-          </>
-        )}
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-[url(../static/budgetHero.png)] bg-cover">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center bg-background  p-10 rounded-lg">
+        <h1 className="text-text-primary text-2xl font-bold">Budget Hero</h1>
+
+        <>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="bg-foreground/20 text-text-primary px-2 py-1 rounded-sm"
+          />
+          <input
+            type="username"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            className="bg-foreground/20 text-text-primary px-2 py-1 rounded-sm"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="bg-foreground/20 text-text-primary px-2 py-1 rounded-sm"
+          />
+          <button
+            className="bg-victory-green text-text-secondary font-semibold w-full rounded-sm p-1 text-lg hover:bg-victory-green/80 active:bg-victory-green/75 cursor-pointer"
+            onClick={handleSignup}
+          >
+            Sign up
+          </button>
+          <a
+            href="/"
+            className="hover:underline hover:underline-offset-4 cursor-pointer"
+          >
+            Already a member?
+          </a>
+        </>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-text-secondary"
           href=""
           target="_blank"
         >
